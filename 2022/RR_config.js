1b6344c497319b03531f25b8fd509bd81f9f8b0b
HaoranJing <43453831+BruceJing13@users.noreply.github.com>
2022-03-21 19:58:22 -0400

    Tippecanoe Scout

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022 Columbus",
+        "defaultValue":"2022 Tippecanoe",
         "required":"true",
         "disabled":"true"
       },
